dfb7836f09b9145e22474ad563d12ded89c0d25f
juckerj <39263956+jonasjucker@users.noreply.github.com>
2025-03-10 14:36:51 +0100

    Add unittests (#7)
    * Create pylint.yml
    
    * add formatting
    
    * rename
    
    * add dummy test
    
    * add pytest
    
    * GitHub Action: Apply Pep8-formatting
    
    * add first set of tests
    
    * GitHub Action: Apply Pep8-formatting
    
    * test more functions
    
    * GitHub Action: Apply Pep8-formatting
    
    * rename test
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- ecmwf.py
+++ ecmwf.py
@@ -7,51 +7,54 @@ import retry
 
 import pandas as pd
 
-
 d10_plume = 'classical_plume'
 d10_eps = 'classical_10d'
 d15_eps = 'classical_15d'
 
-ALL_EPSGRAM = [d10_plume,d10_eps,d15_eps] 
+ALL_EPSGRAM = [d10_plume, d10_eps, d15_eps]
+
 
 class EcmwfApi():
-    def __init__(self,station_config):
-        
+
+    def __init__(self, station_config):
+
         class Station():
-            def __init__(self,name,lat,lon):
+
+            def __init__(self, name, lat, lon):
                 self.name = name
                 self.lat = lat
                 self.lon = lon
                 self.base_time = None
 
-        self._API_URL = "https://charts.ecmwf.int/opencharts-api/v1/" 
-        self._stations = [Station(**station_data) for station_data in station_config]
+        self._API_URL = "https://charts.ecmwf.int/opencharts-api/v1/"
+        self._stations = [
+            Station(**station_data) for station_data in station_config
+        ]
         self._epsgrams = ALL_EPSGRAM
         self._time_format = '%Y-%m-%dT%H:%M:%SZ'
-        self._base_time = self._fetch_available_base_time(fallback=True, timeshift=0)
+        self._base_time = self._fetch_available_base_time(fallback=True,
+                                                          timeshift=0)
 
         self._plots_for_broadcast = {}
 
         # populate stations with valid run
         for Station in self._stations:
             Station.base_time = self._base_time
-            logging.debug('init {} with base_time {}'.format(Station.name,Station.base_time))
-
+            logging.debug('init {} with base_time {}'.format(
+                Station.name, Station.base_time))
 
     def _first_guess_base_time(self):
         t_now = datetime.datetime.now()
         t_now_rounded = pd.Timestamp.now().round(freq='12H').to_pydatetime()
 
-
         # rounding ends up in future
         if t_now <= t_now_rounded:
-            t_now_rounded = t_now_rounded - datetime.timedelta(hours = 12) 
+            t_now_rounded = t_now_rounded - datetime.timedelta(hours=12)
 
         latest_run = t_now_rounded.strftime(self._time_format)
         logging.debug('latest run: {}'.format(latest_run))
 
         return latest_run
-
 
     def upgrade_basetime(self):
         try:
@@ -60,45 +63,48 @@ class EcmwfApi():
                 self._base_time = new_base_time
                 logging.info('base_time updated to {}'.format(self._base_time))
         except ValueError:
-            logging.debug('Upgrading base_time failed, keeping {}'.format(self._base_time))
+            logging.debug('Upgrading base_time failed, keeping {}'.format(
+                self._base_time))
             pass
 
-        self._base_time = self._fetch_available_base_time(fallback=True)
-
-    def _fetch_available_base_time(self,fallback=False, timeshift=0):
+    def _fetch_available_base_time(self, fallback=False, timeshift=0):
         link = "schema/?product=opencharts_meteogram&package=openchart"
         try:
-            run = self._get_from_API(link)['paths']['/products/opencharts_meteogram/']['get']['parameters'][1]['schema']['default']
-            run_datetime = datetime.datetime.strptime(run, self._time_format)
-            run_datetime -= datetime.timedelta(hours=timeshift)
-            run = run_datetime.strftime(self._time_format)
+            run = self._get_from_API(
+                link)['paths']['/products/opencharts_meteogram/']['get'][
+                    'parameters'][1]['schema']['default']
         except ValueError:
             if fallback:
                 run = self._first_guess_base_time()
             else:
                 raise ValueError('No available base_time found')
+        run_datetime = datetime.datetime.strptime(run, self._time_format)
+        run_datetime -= datetime.timedelta(hours=timeshift)
+        run = run_datetime.strftime(self._time_format)
         return run
 
-    def _latest_confirmed_run(self,station):
+    def _latest_confirmed_run(self, station):
         # check if forecast for basetime is available for all epsgrams
         base_time = set()
         for eps_type in self._epsgrams:
             try:
-                self._get_API_data_for_epsgram(station,self._base_time,eps_type,raise_on_error=True)
+                self._get_API_data_for_epsgram(station,
+                                               self._base_time,
+                                               eps_type,
+                                               raise_on_error=True)
                 base_time.add(self._base_time)
             except ValueError as e:
-                base_time.add(station.base_time) 
+                base_time.add(station.base_time)
 
         # if there are multiple base_time, take the oldest
-        if len(base_time)> 1:
+        if len(base_time) > 1:
             return min(base_time)
         else:
             return base_time.pop()
 
-
     @retry.retry(tries=10, delay=3)
-    def _get_from_API(self,link,raise_on_error=True):
-        get = '{}{}'.format(self._API_URL,link)
+    def _get_from_API(self, link, raise_on_error=True):
+        get = '{}{}'.format(self._API_URL, link)
         logging.debug('GET {}'.format(get))
         result = requests.get(get)
 
@@ -108,7 +114,8 @@ class EcmwfApi():
             if result.status_code == 403:
                 logging.info('403 Forbidden for {}'.format(get))
                 raise ValueError('403 Forbidden for {}'.format(get))
-                raise ValueError('403 Forbidden for {} at {}'.format(station.name,base_time))
+                raise ValueError('403 Forbidden for {} at {}'.format(
+                    station.name, base_time))
             else:
                 try:
                     return result.json()
@@ -116,29 +123,32 @@ class EcmwfApi():
                     logging.info('JSONDecodeError for {}'.format(get))
                     raise ValueError('JSONDecodeError for {}'.format(get))
 
-    def _get_API_data_for_epsgram(self,station,base_time,eps_type,raise_on_error=True):
-        link = 'products/opencharts_meteogram/?epsgram={}&base_time={}&station_name={}&lat={}&lon={}'.format( eps_type,
-                                                                                            base_time,
-                                                                                            station.name,
-                                                                                            station.lat,
-                                                                                            station.lon)
+    def _get_API_data_for_epsgram(self,
+                                  station,
+                                  base_time,
+                                  eps_type,
+                                  raise_on_error=True):
+        link = 'products/opencharts_meteogram/?epsgram={}&base_time={}&station_name={}&lat={}&lon={}'.format(
+            eps_type, base_time, station.name, station.lat, station.lon)
 
-        return self._get_from_API(link,raise_on_error=raise_on_error)
+        return self._get_from_API(link, raise_on_error=raise_on_error)
 
-        
-    def _request_epsgram_link_for_station(self,station, eps_type):
-        data = self._get_API_data_for_epsgram(station,station.base_time,eps_type,raise_on_error=True)
+    def _request_epsgram_link_for_station(self, station, eps_type):
+        data = self._get_API_data_for_epsgram(station,
+                                              station.base_time,
+                                              eps_type,
+                                              raise_on_error=True)
         return data["data"]["link"]["href"]
 
-    def _save_image_of_station(self,image_api,station,eps_type):
+    def _save_image_of_station(self, image_api, station, eps_type):
         image = requests.get(image_api)
-        file = "./{}_{}.png".format(station.name,eps_type)
+        file = "./{}_{}.png".format(station.name, eps_type)
         with open(file, "wb") as img:
             img.write(image.content)
         logging.info("image saved in {}".format(file))
         return file
 
-    def download_plots(self,requested_stations):
+    def download_plots(self, requested_stations):
         for Station in self._stations:
             if Station.name in requested_stations:
                 self._download_plots(Station)
@@ -151,30 +161,35 @@ class EcmwfApi():
 
     def download_latest_plots(self):
         for Station in self._stations:
-                if self._new_forecast_available(Station):
+            if self._new_forecast_available(Station):
 
-                    # base_time for which all epsgrams are available
-                    base_time = self._latest_confirmed_run(Station)
-                    base_time_dt = datetime.datetime.strptime(base_time, self._time_format)
+                # base_time for which all epsgrams are available
+                base_time = self._latest_confirmed_run(Station)
+                base_time_dt = datetime.datetime.strptime(
+                    base_time, self._time_format)
 
-                    # base time available from schema request
-                    current_base_time_dt = datetime.datetime.strptime(self._base_time, self._time_format)
-                    
-                    # last confirmed base_time for station
-                    station_base_time_dt = datetime.datetime.strptime(Station.base_time, self._time_format)
+                # base time available from schema request
+                current_base_time_dt = datetime.datetime.strptime(
+                    self._base_time, self._time_format)
 
-                    # if base_time from latest_confirmed_run is different from current base_time
-                    # or station base_time, send plots and upgrade base_time in station
-                    if base_time_dt != current_base_time_dt or base_time_dt != station_base_time_dt:
-                        logging.debug('base_time for {} updated to {}'.format(Station.name,base_time))
+                # last confirmed base_time for station
+                station_base_time_dt = datetime.datetime.strptime(
+                    Station.base_time, self._time_format)
 
-                        # base_time needs update before fetch
-                        # if not updated, bot sends endless plots to users
-                        Station.base_time = base_time
-                        self._download_plots(Station)
-                    else:
-                        logging.debug('base_time for {} {} and {} are the same'.format(Station.name,Station.base_time,base_time))
+                # if base_time from latest_confirmed_run is different from current base_time
+                # or station base_time, send plots and upgrade base_time in station
+                if base_time_dt != current_base_time_dt or base_time_dt != station_base_time_dt:
+                    logging.debug('base_time for {} updated to {}'.format(
+                        Station.name, base_time))
 
+                    # base_time needs update before fetch
+                    # if not updated, bot sends endless plots to users
+                    Station.base_time = base_time
+                    self._download_plots(Station)
+                else:
+                    logging.debug(
+                        'base_time for {} {} and {} are the same'.format(
+                            Station.name, Station.base_time, base_time))
 
         # copy because we reset _plots_for_broadcast now
         plots_for_broadcast = self._plots_for_broadcast.copy()
@@ -182,20 +197,22 @@ class EcmwfApi():
 
         return plots_for_broadcast
 
-
-    def _download_plots(self,Station):
-            logging.info('Fetch plots for {}'.format(Station.name))
+    def _download_plots(self, Station):
+        logging.info('Fetch plots for {}'.format(Station.name))
+        plots = []
+        try:
+            for type in self._epsgrams:
+                image_api = self._request_epsgram_link_for_station(
+                    Station, type)
+                plots.append(
+                    self._save_image_of_station(image_api, Station, type))
+        except ValueError as e:
+            logging.warning(
+                'Error while fetching plots for {}, skipping...'.format(
+                    Station.name))
             plots = []
-            try:
-                for type in self._epsgrams:
-                    image_api =  self._request_epsgram_link_for_station(Station,type)
-                    plots.append(self._save_image_of_station(image_api,Station,type))
-            except ValueError as e:
-                logging.warning('Error while fetching plots for {}, skipping...'.format(Station.name))
-                plots = []
 
-            self._plots_for_broadcast[Station.name] = plots
+        self._plots_for_broadcast[Station.name] = plots
 
-
-    def _new_forecast_available(self,Station):
-            return Station.base_time != self._base_time
+    def _new_forecast_available(self, Station):
+        return Station.base_time != self._base_time
---
+++ test/test_ecmwf.py
@@ -0,0 +1,167 @@
+import pytest
+import yaml
+import sys
+import os
+from unittest.mock import patch
+from datetime import datetime
+
+# Add the parent directory to the sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from ecmwf import EcmwfApi
+
+
+@pytest.fixture
+def ecmwf(station_config):
+    return EcmwfApi(station_config)
+
+
+@pytest.fixture
+def station_config():
+    with open('stations.yaml', 'r') as file:
+        station_config = yaml.safe_load(file)
+    return station_config
+
+
+def test_station_config_has_valid_entries(station_config):
+    for station in station_config:
+        assert 'name' in station and isinstance(
+            station['name'], str), f'{station["name"]} is not a string'
+        assert 'lat' in station and isinstance(
+            station['lat'], float), f'{station["lat"]} is not a float'
+        assert 'lon' in station and isinstance(
+            station['lon'], float), f'{station["lon"]} is not a float'
+
+
+def test_ecmwf_api_init_with_station_config(station_config):
+    ecmwf = EcmwfApi(station_config)
+    assert ecmwf is not None
+
+
+def test_fetch_available_base_time(ecmwf):
+    base_time = ecmwf._fetch_available_base_time(fallback=True, timeshift=0)
+    assert isinstance(base_time, str), "base_time is not a string"
+    try:
+        datetime.strptime(base_time, ecmwf._time_format)
+    except ValueError:
+        pytest.fail(f"base_time '{base_time}' is not a valid datetime")
+
+
+@pytest.mark.parametrize("timeshift", [3, 12, 24, -12])
+def test_fetch_available_base_time_with_timeshift_of(ecmwf, timeshift):
+    base_time_no_shift = ecmwf._fetch_available_base_time(fallback=True,
+                                                          timeshift=0)
+    base_time = ecmwf._fetch_available_base_time(fallback=True,
+                                                 timeshift=timeshift)
+    # check that the time is shifted by the correct amount
+    base_time_dt = datetime.strptime(base_time, ecmwf._time_format)
+    base_time_no_shift_dt = datetime.strptime(base_time_no_shift,
+                                              ecmwf._time_format)
+    assert (base_time_no_shift_dt -
+            base_time_dt).total_seconds() == timeshift * 3600
+
+
+def test_fetch_available_base_time_with_fallback(ecmwf):
+    with patch.object(EcmwfApi, '_get_from_API', side_effect=ValueError):
+        base_time = ecmwf._fetch_available_base_time(fallback=True,
+                                                     timeshift=0)
+        assert isinstance(base_time, str), "base_time is not a string"
+        try:
+            datetime.strptime(base_time, ecmwf._time_format)
+        except ValueError:
+            pytest.fail(f"base_time '{base_time}' is not a valid datetime")
+
+
+def test_fetch_available_base_time_no_fallback(ecmwf):
+    with patch.object(EcmwfApi, '_get_from_API', side_effect=ValueError):
+        with pytest.raises(ValueError, match="No available base_time found"):
+            ecmwf._fetch_available_base_time(fallback=False, timeshift=0)
+
+
+def test_fetch_available_base_time_timeshift_for_fallback(ecmwf):
+    timeshift = 12
+    with patch.object(EcmwfApi, '_get_from_API', side_effect=ValueError):
+        base_time = ecmwf._fetch_available_base_time(fallback=True,
+                                                     timeshift=timeshift)
+        base_time_no_shift = ecmwf._fetch_available_base_time(fallback=True,
+                                                              timeshift=0)
+        base_time_dt = datetime.strptime(base_time, ecmwf._time_format)
+        base_time_no_shift_dt = datetime.strptime(base_time_no_shift,
+                                                  ecmwf._time_format)
+        assert (base_time_no_shift_dt -
+                base_time_dt).total_seconds() == timeshift * 3600
+
+
+def test_upgrade_base_time(ecmwf):
+    # override with based time shifted by 12 hours
+    ecmwf._base_time = ecmwf._fetch_available_base_time(fallback=True,
+                                                        timeshift=12)
+    base_time_shifted = ecmwf._base_time
+    ecmwf.upgrade_basetime()
+    assert ecmwf._base_time != base_time_shifted, "base_time was not updated"
+
+
+def test_upgrade_base_time_if_api_request_fails(ecmwf):
+    # override with based time shifted by 12 hours
+    ecmwf._base_time = ecmwf._fetch_available_base_time(fallback=True,
+                                                        timeshift=12)
+    base_time_shifted = ecmwf._base_time
+    with patch.object(EcmwfApi,
+                      '_fetch_available_base_time',
+                      side_effect=ValueError):
+        ecmwf.upgrade_basetime()
+    assert ecmwf._base_time == base_time_shifted, "base_time was updated but should not have been"
+
+
+def test_new_forecast_available_for_same_basetimes(ecmwf):
+    # station and ecmwd.base_time are set to the same value
+    for Station in ecmwf._stations:
+        assert ecmwf._new_forecast_available(
+            Station
+        ) == False, f"New forecast should not be available for {Station.name}"
+
+
+def test_new_forecast_available_for_different_basetimes(ecmwf):
+    ecmwf._base_time = ecmwf._fetch_available_base_time(fallback=True,
+                                                        timeshift=12)
+    # station and ecmwd.base_time are set to different values
+    for Station in ecmwf._stations:
+        assert ecmwf._new_forecast_available(
+            Station
+        ) == True, f"New forecast should be available for {Station.name}"
+
+
+@pytest.mark.parametrize("station", ['Tschiertschen', 'Elm'])
+def test_latest_confirmed_run_for(ecmwf, station):
+    for Station in ecmwf._stations:
+        if Station.name == station:
+            latest_run = ecmwf._latest_confirmed_run(Station)
+            assert isinstance(latest_run, str), "latest_run is not a string"
+            try:
+                datetime.strptime(latest_run, ecmwf._time_format)
+            except ValueError:
+                pytest.fail(
+                    f"latest_run '{latest_run}' is not a valid datetime")
+
+
+@pytest.mark.parametrize("station", ['Winterthur'])
+def test_latest_confirmed_run_with_base_time_48_h_in_past_for(ecmwf, station):
+    base_time_at_init = ecmwf._base_time
+    ecmwf._base_time = ecmwf._fetch_available_base_time(fallback=True,
+                                                        timeshift=48)
+    for Station in ecmwf._stations:
+        if Station.name == station:
+            latest_run = ecmwf._latest_confirmed_run(Station)
+            assert base_time_at_init != latest_run, "latest_confirmed_run should be in far past"
+            assert ecmwf._base_time == latest_run, "latest_confirmed_run should be identical to base_time"
+
+
+def test_latest_confirmed_run_with_api_fail(ecmwf):
+    for Station in ecmwf._stations:
+        Station.base_time = ecmwf._fetch_available_base_time(fallback=True,
+                                                             timeshift=24)
+        with patch.object(EcmwfApi,
+                          '_get_API_data_for_epsgram',
+                          side_effect=ValueError):
+            latest_run = ecmwf._latest_confirmed_run(Station)
+        assert Station.base_time == latest_run, "latest_confirmed_run should be identical to base_time of station"
